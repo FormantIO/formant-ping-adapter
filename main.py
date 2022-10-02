c45f79f7c0ad5c7e91767d5aa555972b94a84111
jturnshek <turnshek@gmail.com>
2022-10-02 05:51:08 -0700

    run the ping process

--- main.py
+++ main.py
@@ -3,84 +3,10 @@
 import os
 import time
 import json
+import subprocess
 import jsonschema
 
 from formant.sdk.agent.v1 import Client as FormantAgentClient
-
-"""
-# Formant Ping Adapter
-
-This adapter pings a remote host and then posts the result to a Formant stream to keep track of the robot's ping over time.
-
-## Configuration
-
-The adapter requires the following configuration:
-
-`hostname` - The hostname or IP address to ping.
-
-`interval` - The interval in seconds between pings.
-
-`timeout` - The timeout for the ping, in seconds.
-
-`formant_stream` - The name of the stream to post the ping to.
-
-### Example Configuration
-
-```json
-{
-    "hostname": "google.com",
-    "interval": 60,
-    "timeout": 5,
-    "formant_stream": "ping"
-}
-```
-
-### Configuration Schema
-
-```json
-{
-    "$schema": "https://json-schema.org/draft/2020-12/schema",
-    "$id": "https://formant.io/formant_ping_adapter_configuration.schema.json",
-    "title": "Formant Ping Adapter Configuration",
-    "description": "Configuration for the ping adapter.",
-    "type": "object",
-    "properties": {
-        "ping_adapter_configuration": {
-            "description": "The top level wrapper for ping adapter configuration.",
-            "type": "object",
-            "properties": {
-                "hostname": {
-                    "description": "The host to ping.",
-                    "type": "string",
-                    "format": "hostname"
-                },
-                "interval": {
-                    "description": "The interval in seconds between ping attempts.",
-                    "type": "number",
-                    "exclusiveMinimum": 0
-                },
-                "timeout": {
-                    "description": "The timeout in seconds to wait for a response from the host.",
-                    "type": "number",
-                    "minimum": 0
-                },
-                "formant_stream": {
-                    "description": "The Formant stream to send the ping results to.",
-                    "type": "string"
-                }
-            },
-            "required": ["hostname", "frequency", "timeout", "formant_stream"]
-        }
-    },
-    "required": ["ros2_adapter_configuration"]
-}
-```
-
-## Tags
-
-Successful pings will be posted with a `success: true` tag, and failed pings will be posted with a `success: false` tag.
-
-"""
 
 DEFAULT_HOSTNAME = "formant.io"
 DEFAULT_INTERVAL = 10
@@ -100,13 +26,22 @@ class PingAdapter:
         self.config = {}
         self.config_schema = {}
 
+        # Set up the default config params
+        self.hostname = DEFAULT_HOSTNAME
+        self.interval = DEFAULT_INTERVAL
+        self.timeout = DEFAULT_TIMEOUT
+        self.formant_stream = DEFAULT_FORMANT_STREAM
+
         # Set up the adapter
         self.fclient = FormantAgentClient(ignore_throttled=True, ignore_unavailable=True)
         self.fclient.register_config_update_callback(self.update_adapter_configuration)
 
-    def update_adapter_configuration(self):
-        # TODO: stop the ping process
+        # Start the ping process
+        while True:
+            self.ping_host()
+            time.sleep(self.interval)
 
+    def update_adapter_configuration(self):
         # Load config from either the agent's json blob or the config.json file
         try:
             config_blob = json.loads(self.fclient.get_config_blob_data())
@@ -134,6 +69,13 @@ class PingAdapter:
         try:
             jsonschema.validate(config_blob, self.config_schema)
             print("INFO: Validation succeeded")
+
+            # Set the config object to the validated configuration
+            if "ping_adapter_configuration" in config_blob:
+                self.config = config_blob["ping_adapter_configuration"]
+            else:
+                self.config = {}
+
         except Exception as e:
             print("WARNING: Validation failed:", e)
             self.fclient.create_event(
@@ -141,18 +83,17 @@ class PingAdapter:
                 notify=False,
                 severity="warning", 
             )
-            return
 
-        # Set the config object to the validated configuration
-        if "ping_adapter_configuration" in config_blob:
-            self.config = config_blob["ping_adapter_configuration"]
-        else:
+            # If the config is invalid, the default will be used
             self.config = {}
 
+        # Set the config params
+        self.hostname = self.config.get("hostname", DEFAULT_HOSTNAME)
+        self.interval = self.config.get("interval", DEFAULT_INTERVAL)
+        self.timeout = self.config.get("timeout", DEFAULT_TIMEOUT)
+        self.formant_stream = self.config.get("formant_stream", DEFAULT_FORMANT_STREAM)
 
-        # TODO: Create a thread to ping the host
-
-        
+        # Post the startup event
         self.fclient.post_json("adapter.configuration", json.dumps(self.config))
         self.fclient.create_event(
             "Ping Adapter started",
@@ -160,6 +101,42 @@ class PingAdapter:
             severity="info", 
         )
         print("INFO: Posted update event and config")
+
+    def ping_host(self):
+        """
+        Ping the host and post the results to the Formant stream
+        """
+
+        # Ping the host
+        try:
+            response = subprocess.run(
+                ["ping", "-c", "1", "-W", str(self.timeout), self.hostname],
+                capture_output=True,
+                text=True,
+            )
+        except Exception as e:
+            print("ERROR: Ping failed:", e)
+            return
+
+        # Parse the ping response
+        if response.returncode == 0:
+            # Successful ping
+            success = True
+            ping_time = float(response.stdout.split("time=")[1].split(" ms")[0])
+        else:
+            # Failed ping
+            success = False
+            ping_time = None
+
+        # Post the ping result to the Formant stream
+        self.fclient.post_numeric(
+            self.formant_stream,
+            ping_time,
+            tags={
+                "success": str(success),
+                "hostname": str(self.hostname),
+            }
+        )
 
 
 if __name__ == "__main__":
